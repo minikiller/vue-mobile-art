ba0c31642cf86efc0efc9069dcda30872f233b21
juezhan <11991762@qq.com>
2018-03-20 14:05:45 +0800

    修改 数据字典、下拉列表

--- src/views/art/base/ArtDistSelect.vue
+++ src/views/art/base/ArtDistSelect.vue
@@ -1,7 +1,7 @@
 <template lang="pug">
   div.art-select
     select.select(v-model='currentValue')
-      option(value="-1") 请选择
+      option(value="" selected="selected") 请选择
       option(v-for="item in items" v-bind:value="item.value") {{item.label}}
 </template>
 <script type="text/ecmascript-6">
@@ -27,7 +27,7 @@
     data() {
       return {
         items: [],
-        currentValue: this.value,
+        currentValue: this.value || '',
         selectedOptions: []
       }
     },
